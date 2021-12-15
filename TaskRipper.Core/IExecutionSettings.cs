9161266ce604d62a87e8269b9b20c7c2ad125e32
Anthony J <reapsprgm@gmail.com>
2021-12-15 00:01:00 -0500

    Create ExecutionSettings

---
+++ TaskRipper.Core/IExecutionSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IExecutionSettings
+    {
+        int MinThreadCount { get; }
+        int MaxThreadCount { get; }
+
+        int MaxExecutionCount { get; }
+        
+        int MinExecutionCount { get; }
+
+        IExecutionEnvironment ExecutionEnvironment { get; }
+    }
+
+    public class ExecutionSettings : IExecutionSettings
+    {
+        public ExecutionSettings(IExecutionEnvironment executionEnvironment)
+        {
+            ExecutionEnvironment = executionEnvironment;
+        }
+
+        public int MinThreadCount { get; }
+
+        public int MaxThreadCount { get; }
+
+        public int MaxExecutionCount { get; }
+
+        public int MinExecutionCount { get; }
+
+        public IExecutionEnvironment ExecutionEnvironment { get; }
+    }
+}
