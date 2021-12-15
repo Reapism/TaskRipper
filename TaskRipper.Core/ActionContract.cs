8b8f3ef71ac23756b0a171d226d0614e16dc1d46
Anthony J <reapsprgm@gmail.com>
2021-12-15 00:00:28 -0500

    Removed Action from IActionContract so we can generalize to WorkContract

---
+++ TaskRipper.Core/ActionContract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskRipper.Core
+{
+
+    public class ActionContract : IWorkContract
+    {
+        public ActionContract(IExecutionSettings executionSettings, string description, int iterations = 1)
+        {
+            ExecutionSettings = executionSettings ?? throw new ArgumentNullException(nameof(executionSettings));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Iterations = iterations > 0 ? iterations : 1;
+        }
+
+        public string Description { get; }
+
+        public DateTime StartDate { get; }
+
+        public DateTime EndDate { get; }
+
+        public int Iterations { get; }
+
+        public IExecutionSettings ExecutionSettings { get; }
+    }
+}
