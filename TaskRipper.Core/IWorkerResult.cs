aab77d89c7b731a8e2f976abf2553692fbb03055
Anthony J <reapsprgm@gmail.com>
2021-12-14 23:59:46 -0500

    create a WorkerResult

---
+++ TaskRipper.Core/IWorkerResult.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IWorkerResult
+    {
+        IWorkContract WorkContract { get; }
+        TimeSpan Duration { get; }
+        int ThreadsUsed { get; }
+    }
+
+    public class WorkerResult : IWorkerResult
+    {
+        public WorkerResult(IWorkContract workContract, int threadsUsed)
+        {
+            WorkContract = workContract;
+            ThreadsUsed = threadsUsed;
+            Duration = workContract.EndDate - workContract.StartDate;
+        }
+        public IWorkContract WorkContract { get; }
+
+        public TimeSpan Duration { get; }
+
+        public int ThreadsUsed { get; }
+    }
+}
