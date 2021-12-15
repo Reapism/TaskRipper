ce53a54401026b7e65d6af6219d582c9dc38f4e6
Anthony J <reapsprgm@gmail.com>
2021-12-15 00:01:37 -0500

    Create basic test with fluent assertions

---
+++ TaskRipper.Core.Tests.Unit/ActionTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TaskRipper.Core.Tests.Unit
+{
+    public class ActionTest
+    {
+        [Fact]
+        public async Task ExecuteActionWithAction()
+        {
+            var actionContract = new ActionContract(ExecutionSettings, "Test", 1010);
+            var executor = new ActionExecutorAsync(new WorkBalancer());
+            var cancellationToken = new CancellationTokenSource().Token;
+            var result = await executor.ExecuteAsync(actionContract, GetAction(), cancellationToken);
+
+            result.ThreadsUsed.Should().Be(actionContract.ExecutionSettings.ExecutionEnvironment.ThreadCount);
+            
+        }
+
+        private Action GetAction() => new Action(() => Console.Write(Random.Shared.Next(1).ToString()));
+        private IExecutionSettings ExecutionSettings => new ExecutionSettings(new ExecutionEnvironment());}
+    }
