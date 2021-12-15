13e15df32404674dcbc6348ec45908d0151af322
Anthony J <reapsprgm@gmail.com>
2021-12-14 22:27:18 -0500

    IFunctionable

---
+++ TaskRipper.Core/IFunctionable.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IFunctionable<out R>
+    {
+        Func<R> Function { get; }
+    }
+    public interface IFunctionable<in T, out R>
+    {
+        Func<T, R> Function { get; }
+    }
+
+    public interface IFunctionable<in T1, in T2, out R>
+    {
+        Func<T1, T2, R> Function { get; }
+    }
+}
