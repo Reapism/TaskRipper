d34829fdb0bfa1ffb049349ca2cb22b5c761bb01
Anthony J <reapsprgm@gmail.com>
2021-12-14 22:27:26 -0500

    IActionable

---
+++ TaskRipper.Core/IActionContract.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IActionContract : IWorkerParameters, IExecutionEnvironment, IActionable
+    {
+    }
+
+    public interface IActionContract<in T> : IWorkerParameters, IExecutionEnvironment, IActionable<T>
+    {
+    }
+
+    public interface IActionContract<in T1, in T2> : IWorkerParameters, IExecutionEnvironment, IActionable<T1, T2>
+    {
+    }
+}
---
+++ TaskRipper.Core/IActionable.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IActionable
+    {
+        Action Action { get; }
+    }
+    public interface IActionable<in T>
+    {
+        Action<T> Action { get; }
+    }
+
+    public interface IActionable<in T1, in T2>
+    {
+        Action<T1, T2> Action { get; }
+    }
+}
