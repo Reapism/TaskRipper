1843fdf8202d7bd6889dd6f29b71f939ca6c46cb
Anthony J <reapsprgm@gmail.com>
2022-02-17 11:35:25 -0500

    Add IterationsOutOfRangeException

---
+++ TaskRipper.Core/TaskRipperException.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskRipper.Core
+{
+    /// <summary>
+    /// The base exception class for all <see cref="TaskRipper"/>
+    /// exceptions.
+    /// </summary>
+    public class TaskRipperException : Exception
+    {
+        public TaskRipperException(string message)
+            : base(message)
+        {
+        }
+        public TaskRipperException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    public class IterationsOutOfRangeException : TaskRipperException
+    {
+        public IterationsOutOfRangeException(int iterations, string parameterName)
+            : base(message: $"The value of [{iterations}] is out of range. Parameter name [{parameterName}]")
+        {
+        }
+    }
+}
