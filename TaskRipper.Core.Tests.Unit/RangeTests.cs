326cf7de72a72d5b04a087005363677f51bf1ca6
Anthony J <reapsprgm@gmail.com>
2022-02-17 21:30:33 -0500

    Add some tests for the IsInRange extension function

---
+++ TaskRipper.Core.Tests.Unit/RangeTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TaskRipper.Core.Tests.Unit
+{
+    public class RangeTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public RangeTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        //[InlineData(1, 10, 11, true)]
+        [InlineData(1, 10, 2, true)]
+        [InlineData(1, 10, 10, false)] // exclusive
+        [InlineData(1, 10, 9, true)]
+        [InlineData(5, 10, 1, false)]
+        [InlineData(5, 10, 5, true)]
+        [InlineData(5, 10, 4, false)]
+
+        public void IsInRange(int min, int max, int value, bool isInRange)
+        {
+            var range = new Range(min, max);
+            var actualIsInRange = range.IsInRange(value);
+            isInRange.Should().Be(actualIsInRange);
+        }
+    }
+}
---
+++ TaskRipper.Core/RangeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskRipper.Core
+{
+    public static class RangeExtensions
+    {
+        public static bool IsInRange(this Range range, Index value)
+        {
+            return value.Value >= range.Start.Value && value.Value < range.End.Value;
+        }
+    }
+}
