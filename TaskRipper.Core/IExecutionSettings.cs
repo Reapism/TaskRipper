c796a79adfea419f24ffb20e0170772529d6a1c0
Anthony J <reapsprgm@gmail.com>
2022-02-17 21:31:51 -0500

    Execution settings should support ranges
    - tinkering with adding a LoadBalancerOptions enum to tell the balancer what to prioritize

--- TaskRipper.Core/IExecutionSettings.cs
+++ TaskRipper.Core/IExecutionSettings.cs
@@ -1,44 +1,115 @@
 ﻿namespace TaskRipper.Core
 {
+    public enum LoadBalancerOptions
+    {
+        /// <summary>
+        /// Allow TaskRipper to infer the task load based on the number of iterations, and maximum threads available
+        /// and chooses how to balance the load of the task.
+        /// <para>Only choose this option if don't understand the load of each iterable task.</para>
+        /// </summary>
+        Optimize,
+
+        /// <summary>
+        /// Split the iterations up in a way where the work is divided proportionally,
+        /// to each thread while using the maximum number of threads available.
+        /// <para>
+        /// Choose this option for a task to iterate 
+        /// </para>
+        /// </summary>
+        Fair,
+
+        /// <summary>
+        /// For low iterations, use maximum number of threads available.
+        /// <para>
+        /// Choose this option for a task with as little iterations as there are threads
+        /// to maximize concurrency.
+        /// </para>
+        /// </summary>
+        Heavy,
+
+        /// <summary>
+        /// Split up the iterations on half as many threads are available.
+        /// </summary>
+        Moderate,
+
+        /// <summary>
+        /// Choose this option to iterate on a single thread.
+        /// </summary>
+        None,
+    }
     public interface IExecutionSettings
     {
-        int MinThreadCount { get; }
-        int MaxThreadCount { get; }
+        /// <summary>
+        /// Gets a <see cref="Range"/> indicating the
+        /// minimum and maximum number of threads
+        /// to allow and use.
+        /// </summary>
+        Range ThreadRange{ get; }
 
-        int MaxExecutionCount { get; }
+        /// Gets a <see cref="Range"/> indicating the
+        /// minimum and maximum number of iterations
+        /// to allow.
+        Range ExecutionRange { get; }
         
-        int MinExecutionCount { get; }
-
+        /// <summary>
+        /// The <see cref="IExecutionEnvironment"/>.
+        /// </summary>
         IExecutionEnvironment ExecutionEnvironment { get; }
+
+        LoadBalancerOptions LoadBalancerOptions { get; }
     }
 
     public class ExecutionSettings : IExecutionSettings
     {
-        private static IExecutionSettings defaultInstance;
+        private static IExecutionSettings? defaultInstance;
+        /// <summary>
+        /// Gets a default instance of the <see cref="IExecutionSettings"/>
+        /// </summary>
+        /// <remarks>
+        /// * Default <see cref="IExecutionSettings.ThreadRange"/> is (1 - <see cref="IExecutionEnvironment.ThreadCount"/>)
+        /// <para>
+        /// * Default <see cref="IExecutionSettings.ExecutionRange"/> is (1 - <see cref="IExecutionEnvironment.ThreadCount"/> * 1000)
+        /// </para>
+        /// </remarks>
         public static IExecutionSettings Default
         {
             get 
             {
                 if (defaultInstance is null)
-                    defaultInstance = new ExecutionSettings(new ExecutionEnvironment());
+                {
+                    var executionEnv = new ExecutionEnvironment();
+                    var threadRange = new Range(1, executionEnv.ThreadCount);
+                    var maxExecutionRange = threadRange.End.Value * 1000;
+                    var executionRange = new Range(1, maxExecutionRange);
+                    defaultInstance = new ExecutionSettings(executionEnv, threadRange, executionRange);
+
+                }
 
                 return defaultInstance;
             }
         }
 
-        public ExecutionSettings(IExecutionEnvironment executionEnvironment)
+        public ExecutionSettings(IExecutionEnvironment executionEnvironment, Range threadRange, Range executionRange)
         {
+            ThreadRange = threadRange;
+            ExecutionRange = executionRange;
             ExecutionEnvironment = executionEnvironment;
         }
 
-        public int MinThreadCount { get; }
+        public ExecutionSettings(Range threadRange, Range executionRange)
+            : this(new ExecutionEnvironment(), threadRange, executionRange)
+        {
+        }
 
-        public int MaxThreadCount { get; }
+        /// <inheritdoc/>
+        public Range ThreadRange { get; }
 
-        public int MaxExecutionCount { get; }
+        /// <inheritdoc/>
+        public Range ExecutionRange { get; }
 
-        public int MinExecutionCount { get; }
-
+        /// <inheritdoc/>
         public IExecutionEnvironment ExecutionEnvironment { get; }
+
+        public LoadBalancerOptions LoadBalancerOptions { get; }
     }
 }
