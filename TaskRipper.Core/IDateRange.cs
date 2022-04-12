ceca7a960d9bd1e0e941c6c39e206cf7bf8a7dba
Anthony J <reapsprgm@gmail.com>
2022-04-12 01:04:38 -0400

    DateRange
    - Duration support

--- TaskRipper.Core/IDateRange.cs
+++ TaskRipper.Core/IDateRange.cs
@@ -7,16 +7,24 @@
         DateTime EndDate { get; }
     }
 
-    //TODO ensure data is correct by asserting using Guard.
+    // TODO ensure data is correct by asserting using Guard.
+    // TODO finish the stubs
     public class DateRange : IDateRange
     {
         public DateRange(DateTime startDate, DateTime endDate)
         {
+            if (endDate < startDate)
+            {
+                throw new DateOutOfRangeException(startDate, endDate);
+            }
+
             StartDate = startDate;
             EndDate = endDate;
         }
         public DateTime StartDate { get; }
 
         public DateTime EndDate { get; }
+
+        public TimeSpan Duration { get { return EndDate - StartDate; } }
     }
 }
