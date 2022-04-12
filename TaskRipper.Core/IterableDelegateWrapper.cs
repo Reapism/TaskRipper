88c99dabcc92a0be46f1eff415dfd71eec0cf671
Anthony J <reapsprgm@gmail.com>
2022-04-12 01:03:58 -0400

    IterableDelegateWrapper
    - Wrap, and WrapAndExecute functions to create Actions/Funcs that are wrapped in chunks to be performed in smaller chunks.

--- TaskRipper.Core/IterableDelegateWrapper.cs
+++ TaskRipper.Core/IterableDelegateWrapper.cs
@@ -6,6 +6,117 @@
     /// </summary>
     internal class IterableDelegateWrapper
     {
+        internal static Task WrapAndExecute(Action action, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            var wrappedAction = new Action(() =>
+            {
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    action();
+                }
+            });
+
+            var wrappedTask = new Task(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
+        internal static Task WrapAndExecute<T>(Action<T> action, T param, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            var wrappedAction = new Action(() =>
+            {
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    action(param);
+                }
+            });
+
+            var wrappedTask = new Task(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
+        internal static Task WrapAndExecute<T1, T2>(Action<T1, T2> action, T1 param, T2 param2, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            var wrappedAction = new Action(() =>
+            {
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    action(param, param2);
+                }
+            });
+
+            var wrappedTask = new Task(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedAction, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
+        internal static Task<IEnumerable<TResult>> WrapAndExecute<TResult>(Func<TResult> func, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            // need a wrapped function to store the value of each func return and
+            // enqueue it. 
+            // then start the task. and expect the IEnumerable<TReturn> back.
+            var wrappedFunc = new Func<IEnumerable<TResult>>(() =>
+            {
+                var funcReturns = new Queue<TResult>(iterationsForThisTask);
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    funcReturns.Enqueue(func());
+                }
+                return funcReturns;
+            });
+
+            var wrappedTask = new Task<IEnumerable<TResult>>(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
+        internal static Task<IEnumerable<TResult>> WrapAndExecute<T, TResult>(Func<T, TResult> func, T param, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            // need a wrapped function to store the value of each func return and
+            // enqueue it. 
+            // then start the task. and expect the IEnumerable<TReturn> back.
+            var wrappedFunc = new Func<IEnumerable<TResult>>(() =>
+            {
+                var funcReturns = new Queue<TResult>(iterationsForThisTask);
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    funcReturns.Enqueue(func(param));
+                }
+                return funcReturns;
+            });
+
+            var wrappedTask = new Task<IEnumerable<TResult>>(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
+        internal static Task<IEnumerable<TResult>> WrapAndExecute<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 param, T2 param2, int iterationsForThisTask, CancellationToken cancellationToken)
+        {
+            // need a wrapped function to store the value of each func return and
+            // enqueue it. 
+            // then start the task. and expect the IEnumerable<TReturn> back.
+            var wrappedFunc = new Func<IEnumerable<TResult>>(() =>
+            {
+                var funcReturns = new Queue<TResult>(iterationsForThisTask);
+                for (var i = 0; i < iterationsForThisTask; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    funcReturns.Enqueue(func(param, param2));
+                }
+                return funcReturns;
+            });
+
+            var wrappedTask = new Task<IEnumerable<TResult>>(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning);
+            var result = Task.Factory.StartNew(wrappedFunc, cancellationToken, TaskCreationOptions.LongRunning, Task.Factory.Scheduler ?? TaskScheduler.Default);
+            return wrappedTask;
+        }
+
         internal static Task Wrap(Action action, int iterationsForThisTask, CancellationToken cancellationToken)
         {
             var wrappedAction = new Action(() =>
