d39177fbf981993a8e493cb32ade7182eae0385e
Anthony J <reapsprgm@gmail.com>
2022-04-12 01:03:15 -0400

    TaskRetriever
    Get the Tasks to execute which wrap the original task in smaller execution chunks to speed up the work

---
+++ TaskRipper.Core/TaskRetriever.cs
@@ -0,0 +1,81 @@
+﻿namespace TaskRipper.Core
+{
+    internal class TaskRetriever
+    {
+        internal static IEnumerable<Task> GetTasks(Action action, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(action, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+
+        internal static IEnumerable<Task> GetTasks<T>(Action<T> action, T param, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(action, param, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+        internal static IEnumerable<Task> GetTasks<T1, T2>(Action<T1, T2> action, T1 param, T2 param2, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(action, param, param2, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+
+        internal static IEnumerable<Task> GetTasks<TResult>(Func<TResult> func, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(func, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+        internal static IEnumerable<Task> GetTasks<T, TResult>(Func<T, TResult> func, T param, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(func, param, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+
+        internal static IEnumerable<Task> GetTasks<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 param, T2 param2, IDictionary<int, int> iterationsByThread, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < iterationsByThread.Count; i++)
+            {
+                var iterableTask = IterableDelegateWrapper.Wrap(func, param, param2, iterationsByThread[i], cancellationToken);
+                tasks.Add(iterableTask);
+            }
+
+            return tasks;
+        }
+    }
+}
