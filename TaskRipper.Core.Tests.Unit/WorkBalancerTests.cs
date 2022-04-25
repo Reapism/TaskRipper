73d499a95804757f146ae20c59a583f833ee2abf
Anthony J <reapsprgm@gmail.com>
2022-04-25 01:07:55 -0400

    Update tests for different WorkBalancerOptions

--- TaskRipper.Core.Tests.Unit/WorkBalancerTests.cs
+++ TaskRipper.Core.Tests.Unit/WorkBalancerTests.cs
@@ -1,4 +1,7 @@
 ﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace TaskRipper.Core.Tests.Unit
@@ -6,32 +9,55 @@ namespace TaskRipper.Core.Tests.Unit
     public class WorkBalancerTests
     {
         [Theory]
-        [InlineData(1, 1, 1, 1)]
-        [InlineData(1, 1, 2, 2)]
-        [InlineData(1, 1, 3, 3)]
-        [InlineData(2, 2, 3, 1)]
-        [InlineData(10, 10, 3, 3)]
-        public void BalanceShouldReturnCorrectValues(int iterations, int expectedIterations, int numberOfThreads, int expectedNumberOfThreads)
+        [InlineData(1, 1, 1, 1, 1)]
+        [InlineData(1, 1, 2, 2, 2)]
+        [InlineData(1, 1, 3, 3, 3)]
+        [InlineData(2, 2, 3, 3, 1)]
+        [InlineData(10, 10, 3, 3, 3)]
+        public void OptimizeShouldReturnCorrectValues(int iterations, int expectedIterations, int minThreadCount, int maxThreadCount, int expectedNumberOfThreads)
         {
-            var workContract = WorkContract.Create("TEST", iterations);
+            var executionSettings = GetExecutionSettings(iterations, minThreadCount, maxThreadCount, WorkBalancerOptions.Optimize);
+            var workContract = GetWorkContract(executionSettings, iterations);
             var actualIterationsByThread = GetWorkBalancer().Balance(workContract);
 
-            if (iterations <= numberOfThreads)
+            if (iterations <= maxThreadCount)
                 actualIterationsByThread.Count.Should().Be(iterations);
             else
                 // Should be the same number of the number of threads.
                 actualIterationsByThread.Count.Should().Be(expectedNumberOfThreads);
 
-            int actualTotalCount = 0;
-            foreach (var kvp in actualIterationsByThread)
-            {
-                actualTotalCount += kvp.Value;
-            }
+            int actualNumberOfIterationsAfterBalance = actualIterationsByThread.Values.As<IEnumerable<int>>().Sum();
 
             // Should be same number of iterations after balancing.
-            actualTotalCount.Should().Be(expectedIterations);
-
+            actualNumberOfIterationsAfterBalance.Should().Be(expectedIterations);
         }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, 1)]
+        [InlineData(1, 1, 1, 2, 1)]
+        [InlineData(1, 1, 1, 3, 1)]
+        [InlineData(2, 2, 1, 3, 1)]
+        [InlineData(10, 10, 1, 3, 1)]
+        public void NoneShouldReturnCorrectValues(int iterations, int expectedIterations, int minThreadCount, int maxThreadCount, int expectedNumberOfThreads)
+        {
+            var executionSettings = GetExecutionSettings(iterations, minThreadCount, maxThreadCount, WorkBalancerOptions.None);
+            var workContract = GetWorkContract(executionSettings, iterations);
+            var actualIterationsByThread = GetWorkBalancer().Balance(workContract);
+
+            actualIterationsByThread.Count.Should().Be(iterations);
+            int actualNumberOfIterationsAfterBalance = actualIterationsByThread.Values.As<IEnumerable<int>>().Sum();
+
+            // Should be same number of iterations after balancing.
+            actualNumberOfIterationsAfterBalance.Should().Be(expectedIterations);
+        }
+
+        // Private Setup Methods
+
+        private static IExecutionSettings GetExecutionSettings(int iterations, int minThreadCount, int maxThreadCount, WorkBalancerOptions workBalancerOptions)
+        {
+            return new ExecutionSettings(new Range(minThreadCount, maxThreadCount), new Range(1, ++iterations), workBalancerOptions);
+        }
+
         private IWorkBalancer workBalancer;
         private IWorkBalancer GetWorkBalancer()
         {
@@ -39,6 +65,11 @@ namespace TaskRipper.Core.Tests.Unit
                 workBalancer = new WorkBalancer();
 
             return workBalancer;
+        }
+
+        private IWorkContract GetWorkContract(IExecutionSettings executionSettings, int iterations)
+        {
+            return WorkContract.Create(executionSettings, "TEST", iterations);
         }
     }
 }
