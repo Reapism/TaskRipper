df468bc951be67166def7afe67c018cc4a328f1b
Anthony J <reapsprgm@gmail.com>
2022-04-25 01:07:34 -0400

    IterationsOutOfRangeException
    update message

--- TaskRipper.Core/TaskRipperException.cs
+++ TaskRipper.Core/TaskRipperException.cs
@@ -19,7 +19,7 @@
     public class IterationsOutOfRangeException : TaskRipperException
     {
         public IterationsOutOfRangeException(IWorkContract workContract, int iterations, string parameterName)
-            : base(message: $"The value of [{iterations}] is out of range. Parameter name [{parameterName}]")
+            : base(message: $"The value of [{iterations}] is out of current execution range of [{workContract.ExecutionSettings.ExecutionRange.Start}..{workContract.ExecutionSettings.ExecutionRange.End}]. Parameter name [{parameterName}]")
         {
         }
     }
