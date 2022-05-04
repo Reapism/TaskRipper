e859eb854ced76c15bb053c7e5d7e207270d1a4f
Anthony J <reapsprgm@gmail.com>
2022-05-04 00:36:36 -0400

    rename

--- TaskRipper.Core/IWorkExecutor.cs
+++ TaskRipper.Core/IWorkExecutor.cs
@@ -1,5 +1,9 @@
 ﻿namespace TaskRipper.Core
 {
+    //TODO add a new Execute overload that takes in a func that helps mutate parameter values before or after an execution of a specific iteration
+    // of running the action/func.
+    // maybe instead of taking in a Action or Func delegate directly into the executor, it takes in a wrapper class
+    // that has the exeuting Action and the action that mutates the value of the parameters if needed.
     public interface IWorkExecutor
     {
         /// <summary>
@@ -8,8 +12,8 @@
         /// <param name="workContract">A <see cref="IWorkContract"/> to describe the workload.</param>
         /// <param name="action">The <see cref="Action"/> to execute.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the action.</param>
-        /// <returns>A <see cref="IWorkerResult"/> containing information about the result.</returns>
-        Task<IWorkerResult> ExecuteAsync(IWorkContract workContract, Action action, CancellationToken cancellationToken);
+        /// <returns>A <see cref="IWorkResult"/> containing information about the result.</returns>
+        Task<IWorkResult> ExecuteAsync(IWorkContract workContract, Action action, CancellationToken cancellationToken);
 
         /// <summary>
         /// Executes a cancellable, <see cref="Action{T}"/> asynchronously using information from a <see cref="IWorkContract"/>.
@@ -18,8 +22,8 @@
         /// <param name="workContract">A <see cref="IWorkContract"/> to describe the workload.</param>
         /// <param name="action">The <see cref="Action{T}"/> to execute.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the action.</param>
-        /// <returns>A <see cref="IWorkerResult"/> containing information about the result.</returns>
-        Task<IWorkerResult> ExecuteAsync<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken);
+        /// <returns>A <see cref="IWorkResult"/> containing information about the result.</returns>
+        Task<IWorkResult> ExecuteAsync<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken);
 
         /// <summary>
         /// Executes a cancellable, <see cref="Action{T1, T2}"/> asynchronously using information from a <see cref="IWorkContract"/>.
@@ -29,11 +33,11 @@
         /// <param name="workContract">A <see cref="IWorkContract"/> to describe the workload.</param>
         /// <param name="action">The <see cref="Action{T1, T2}"/> to execute.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the action.</param>
-        /// <returns>A <see cref="IWorkerResult"/> containing information about the result.</returns>
-        Task<IWorkerResult> ExecuteAsync<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken);
-        Task<IWorkerResult<TResult>> ExecuteAsync<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken);
-        Task<IWorkerResult<TResult>> ExecuteAsync<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken);
-        Task<IWorkerResult<TResult>> ExecuteAsync<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken);
+        /// <returns>A <see cref="IWorkResult"/> containing information about the result.</returns>
+        Task<IWorkResult> ExecuteAsync<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken);
+        Task<IWorkResult<TResult>> ExecuteAsync<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken);
+        Task<IWorkResult<TResult>> ExecuteAsync<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken);
+        Task<IWorkResult<TResult>> ExecuteAsync<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken);
     }
 
     public class WorkExecutor : IWorkExecutor
@@ -56,37 +60,37 @@
             }
         }
 
-        public async Task<IWorkerResult> ExecuteAsync(IWorkContract workContract, Action action, CancellationToken cancellationToken)
+        public async Task<IWorkResult> ExecuteAsync(IWorkContract workContract, Action action, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncActionInternal(workContract, action, cancellationToken);
         }
 
-        public async Task<IWorkerResult> ExecuteAsync<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken)
+        public async Task<IWorkResult> ExecuteAsync<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncActionInternal(workContract, action, param, cancellationToken);
         }
 
-        public async Task<IWorkerResult> ExecuteAsync<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken)
+        public async Task<IWorkResult> ExecuteAsync<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncActionInternal(workContract, action, param, param2, cancellationToken);
         }
 
-        public async Task<IWorkerResult<TResult>> ExecuteAsync<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken)
+        public async Task<IWorkResult<TResult>> ExecuteAsync<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncFuncInternal(workContract, func, cancellationToken);
         }
 
-        public async Task<IWorkerResult<TResult>> ExecuteAsync<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken)
+        public async Task<IWorkResult<TResult>> ExecuteAsync<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncFuncInternal(workContract, func, param, cancellationToken);
         }
 
-        public async Task<IWorkerResult<TResult>> ExecuteAsync<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken)
+        public async Task<IWorkResult<TResult>> ExecuteAsync<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken)
         {
             return await ExecuteAsyncFuncInternal(workContract, func, param, param2, cancellationToken);
         }
 
-        private async Task<IWorkerResult> ExecuteAsyncActionInternal(IWorkContract workContract, Action action, CancellationToken cancellationToken)
+        private async Task<IWorkResult> ExecuteAsyncActionInternal(IWorkContract workContract, Action action, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, action);
 
@@ -101,10 +105,10 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult(workContract, tasks.Count(), dateRange));
+            return await Task.FromResult(new WorkResult(workContract, tasks.Count(), dateRange));
         }
 
-        private async Task<IWorkerResult> ExecuteAsyncActionInternal<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken)
+        private async Task<IWorkResult> ExecuteAsyncActionInternal<T>(IWorkContract workContract, Action<T> action, T param, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, action);
 
@@ -119,10 +123,10 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult(workContract, tasks.Count(), dateRange));
+            return await Task.FromResult(new WorkResult(workContract, tasks.Count(), dateRange));
         }
 
-        private async Task<IWorkerResult> ExecuteAsyncActionInternal<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken)
+        private async Task<IWorkResult> ExecuteAsyncActionInternal<T1, T2>(IWorkContract workContract, Action<T1, T2> action, T1 param, T2 param2, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, action);
 
@@ -137,10 +141,10 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult(workContract, tasks.Count(), dateRange));
+            return await Task.FromResult(new WorkResult(workContract, tasks.Count(), dateRange));
         }
 
-        private async Task<IWorkerResult<TResult>> ExecuteAsyncFuncInternal<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken)
+        private async Task<IWorkResult<TResult>> ExecuteAsyncFuncInternal<TResult>(IWorkContract workContract, Func<TResult> func, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, func);
 
@@ -155,10 +159,10 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult<TResult>(workContract, tasks.Count(), dateRange, null));
+            return await Task.FromResult(new WorkResult<TResult>(workContract, tasks.Count(), dateRange, null));
         }
 
-        private async Task<IWorkerResult<TResult>> ExecuteAsyncFuncInternal<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken)
+        private async Task<IWorkResult<TResult>> ExecuteAsyncFuncInternal<T, TResult>(IWorkContract workContract, Func<T, TResult> func, T param, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, func);
 
@@ -173,17 +177,17 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult<TResult>(workContract, tasks.Count(), dateRange, null));
+            return await Task.FromResult(new WorkResult<TResult>(workContract, tasks.Count(), dateRange, null));
         }
 
-        private async Task<IWorkerResult<TResult>> ExecuteAsyncFuncInternal<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken)
+        private async Task<IWorkResult<TResult>> ExecuteAsyncFuncInternal<T1, T2, TResult>(IWorkContract workContract, Func<T1, T2, TResult> func, T1 param, T2 param2, CancellationToken cancellationToken)
         {
             ValidateContract(workContract, func);
 
             var startDate = DateTime.Now;
 
             var iterationsByThread = workBalancer.Balance(workContract);
-            var tasks = TaskRetriever.GetTasks(func, param, param2, iterationsByThread, cancellationToken);
+            var tasks = IterableDelegateWrapper.WrapAndExecute(func, param, param2, iterationsByThread, cancellationToken);
 
             StartTasks(tasks);
             await WaitForAllTasks(tasks);
@@ -191,7 +195,7 @@
             var endDate = DateTime.Now;
 
             var dateRange = new DateRange(startDate, endDate);
-            return await Task.FromResult(new WorkerResult<TResult>(workContract, tasks.Count(), dateRange, null));
+            return await Task.FromResult(new WorkResult<TResult>(workContract, tasks.Count(), dateRange, null));
         }
 
         private static void HandleIncompleteTasks(IEnumerable<Task> tasks)
