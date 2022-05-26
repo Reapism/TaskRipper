af7b424564db4607d181ac0e51e6d5a6e0594644
Anthony J <reapsprgm@gmail.com>
2022-05-26 00:52:27 -0400

    Add work contract tests

---
+++ TaskRipper.Core.Tests.Unit/WorkContractTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TaskRipper.Core.Tests.Unit
+{
+    public class WorkContractTests
+    {
+        [Theory]
+        [InlineData("", "", 1, 1, true)]
+        [InlineData("a", "a", 1, 1, true)]
+        [InlineData("a", "A", 1, 1, false)]
+        [InlineData("a", "A", 1, 0, false)]
+        [InlineData("a", "a", 1, 0, false)]
+        public void ShouldBeEqual(string desc1, string desc2, int iter1, int iter2, bool shouldBeEqual)
+        {
+            IWorkContract firstContract = WorkContract.Create(GetExecutionSettings(), desc1, iter1);
+            IWorkContract secondContract = WorkContract.Create(GetExecutionSettings(), desc2, iter2);
+
+            if (shouldBeEqual)
+                firstContract.Should().Be(secondContract);
+            else
+                firstContract.Should().NotBe(secondContract);
+        }
+
+        private IExecutionSettings GetExecutionSettings()
+        {
+            return ExecutionSettings.Default;
+        }
+    }
+}
