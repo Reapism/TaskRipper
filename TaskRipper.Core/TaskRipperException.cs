770e46d3894e1fd9c17971f124525d36e7d6114b
Anthony J <reapsprgm@gmail.com>
2022-05-26 00:51:40 -0400

    Add TypeMismatchException and a potential exception RecursiveStackOverflowException

--- TaskRipper.Core/TaskRipperException.cs
+++ TaskRipper.Core/TaskRipperException.cs
@@ -39,4 +39,26 @@
 
         }
     }
+
+    public class TypeMismatchException : TaskRipperException
+    {
+        public TypeMismatchException(Type expectedType, Type actualType)
+            : base(message:$"The expected type {expectedType.FullName} does not match the actual {actualType.FullName}.")
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// If its possible to detect that a function will not terminate simply by looking at the base cases that cannot be reached
+    /// like if the base case is -1 but the function only increments values?
+    /// </summary>
+    public class RecursiveStackOverflowException : TaskRipperException
+    {
+        public RecursiveStackOverflowException(Type type)
+            : base(message: $"The following type {type.FullName} has a function that recursively creates more functions that will not terminate.")
+        {
+
+        }
+    }
 }
