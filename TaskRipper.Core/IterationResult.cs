a93beb2a59c00e47f8091d0666e1d87d19713d69
Anthony J <reapsprgm@gmail.com>
2022-07-08 02:18:36 -0400

    IterationResult has debugger display attribute

--- TaskRipper.Core/IterationResult.cs
+++ TaskRipper.Core/IterationResult.cs
@@ -1,13 +1,18 @@
-﻿namespace TaskRipper.Core
+﻿using System.Diagnostics;
+
+namespace TaskRipper.Core
 {
+    [DebuggerDisplay("[Thread # {ThreadNumber} Iteration {Iteration}]: {Result}")]
     public class IterationResult<TResult>
     {
-        public IterationResult(int iteration, TResult result)
+        public IterationResult(int threadNumber, int iteration, TResult result)
         {
+            ThreadNumber = threadNumber;
             Iteration = iteration;
             Result = result;
         }
 
+        public int ThreadNumber { get; }
         public int Iteration { get; }
         public TResult Result { get; }
     }
