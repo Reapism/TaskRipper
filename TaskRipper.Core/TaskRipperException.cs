8f152d15b8f89093ad234ed89d628170febcbceb
Anthony J <reapsprgm@gmail.com>
2022-07-08 02:17:53 -0400

    add thread out of range exc

--- TaskRipper.Core/TaskRipperException.cs
+++ TaskRipper.Core/TaskRipperException.cs
@@ -16,27 +16,32 @@
         }
     }
 
-    public class IterationsOutOfRangeException : TaskRipperException
+    public class ThreadOutOfRangeException : TaskRipperException
     {
-        public IterationsOutOfRangeException(IWorkContract workContract, int iterations, string parameterName)
-            : base(message: $"The value of [{iterations}] is out of current execution range of [{workContract.ExecutionSettings.ExecutionRange.Start}..{workContract.ExecutionSettings.ExecutionRange.End}]. Parameter name [{parameterName}]")
+        public ThreadOutOfRangeException(IWorkContract workContract, int threadCount, string parameterName)
+            : base(message: $"The value of [{threadCount}] is out of the work contracts thread range of [{workContract.ExecutionSettings.ThreadRange.Start}..{workContract.ExecutionSettings.ThreadRange.End}]. Parameter name [{parameterName}]")
         {
         }
     }
 
+    public class IterationsOutOfRangeException : TaskRipperException
+    {
+        public IterationsOutOfRangeException(IWorkContract workContract, int iterations, string parameterName)
+            : base(message: $"The value of [{iterations}] is out of execution range of [{workContract.ExecutionSettings.ExecutionRange.Start}..{workContract.ExecutionSettings.ExecutionRange.End}]. Parameter name [{parameterName}]")
+        {
+        }
+    }
 
     public class DateOutOfRangeException : TaskRipperException
     {
         public DateOutOfRangeException(IDateRange dateRange)
             : base(message: $"The end date: [{dateRange.EndDate}] must be greater than the start date: [{dateRange.StartDate}].")
         {
-
         }
 
         public DateOutOfRangeException(DateTime startDate, DateTime endDate)
             : base(message: $"The end date: [{endDate}] must be greater than the start date: [{startDate}].")
         {
-
         }
     }
 
@@ -45,20 +50,6 @@
         public TypeMismatchException(Type expectedType, Type actualType)
             : base(message:$"The expected type {expectedType.FullName} does not match the actual {actualType.FullName}.")
         {
-            
-        }
-    }
-
-    /// <summary>
-    /// If its possible to detect that a function will not terminate simply by looking at the base cases that cannot be reached
-    /// like if the base case is -1 but the function only increments values?
-    /// </summary>
-    public class RecursiveStackOverflowException : TaskRipperException
-    {
-        public RecursiveStackOverflowException(Type type)
-            : base(message: $"The following type {type.FullName} has a function that recursively creates more functions that will not terminate.")
-        {
-
         }
     }
 }
