20a8a3ce0f46c403f9386286bdbe67915f9d886f
Anthony J <reapsprgm@gmail.com>
2022-07-08 02:18:20 -0400

    add new portion of validating a work contract for thread range

--- TaskRipper.Core/WorkContractExtensions.cs
+++ TaskRipper.Core/WorkContractExtensions.cs
@@ -5,7 +5,7 @@
         internal static void ValidateContract(this IWorkContract contract)
         {
             // Ensure all dependencies are non null
-            if (contract == null)
+            if (contract is null)
                 throw new ArgumentNullException(nameof(contract));
 
             if (contract.ExecutionSettings is null)
@@ -15,10 +15,17 @@
                 throw new ArgumentNullException(nameof(contract.ExecutionSettings.ExecutionEnvironment));
 
             // Ensure iterations is within execution range.
-            if (contract.Iterations > contract.ExecutionSettings.ExecutionRange.End.Value ||
-                contract.Iterations <= contract.ExecutionSettings.ExecutionRange.Start.Value)
+            if (!contract.ExecutionSettings.ExecutionRange.IsInRange(contract.Iterations))
             {
                 throw new IterationsOutOfRangeException(contract, contract.Iterations, nameof(contract.Iterations));
+            }
+
+            // Ensure the execution environment thread count is in range of the declared thread range in the contract.
+            if (!contract.ExecutionSettings.ThreadRange.IsInRange(contract.ExecutionSettings.ExecutionEnvironment.ThreadCount))
+            {
+                throw new ThreadOutOfRangeException(contract,
+                    contract.ExecutionSettings.ExecutionEnvironment.ThreadCount,
+                    nameof(contract.ExecutionSettings.ExecutionEnvironment.ThreadCount));
             }
         }
     }
