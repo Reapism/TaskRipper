7432fd0327cf852096f79acb60678883af5c0ecd
Anthony J <reapsprgm@gmail.com>
2022-08-27 00:16:14 -0400

    Add doc

--- TaskRipper.Core/WorkContractExtensions.cs
+++ TaskRipper.Core/WorkContractExtensions.cs
@@ -2,6 +2,15 @@
 {
     internal static class IWorkContractExtensions
     {
+        /// <summary>
+        /// Validates a <see cref="IWorkContract"/>.
+        /// Ensures all dependencies are non null, and are
+        /// in range.
+        /// </summary>
+        /// <param name="contract">The <see cref="IWorkContract"/> instance to validate.</param>
+        /// <exception cref="ArgumentNullException">If any dependency is null.</exception>
+        /// <exception cref="IterationsOutOfRangeException">Thrown if the iteration are out of range.</exception>
+        /// <exception cref="ThreadOutOfRangeException">Thrown if the environment thread count is out of range.</exception>
         internal static void ValidateContract(this IWorkContract contract)
         {
             // Ensure all dependencies are non null
