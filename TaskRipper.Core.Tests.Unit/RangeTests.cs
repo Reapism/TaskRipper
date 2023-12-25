33e7ab421dd5ed7e45302913baf410d79bf9db9c
Anthony J <reapsprgm@gmail.com>
2023-12-25 00:56:12 -0500

    Fix range test and reorder them, original logic is fine

--- TaskRipper.Core.Tests.Unit/RangeTests.cs
+++ TaskRipper.Core.Tests.Unit/RangeTests.cs
@@ -15,13 +15,13 @@ namespace TaskRipper.Core.Tests.Unit
         }
 
         [Theory]
-        //[InlineData(1, 10, 11, true)]
+        [InlineData(1, 10, 11, false)]
         [InlineData(1, 10, 2, true)]
-        [InlineData(1, 10, 10, false)] // exclusive
         [InlineData(1, 10, 9, true)]
+        [InlineData(1, 10, 10, false)] // exclusive
         [InlineData(5, 10, 1, false)]
-        [InlineData(5, 10, 5, true)]
         [InlineData(5, 10, 4, false)]
+        [InlineData(5, 10, 5, true)]
         public void IsInRange(int min, int max, int value, bool isInRange)
         {
             Range range = new Range(min, max);
