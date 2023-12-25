07f19440182822042d5ae63957effb2d0064471a
Anthony J <reapsprgm@gmail.com>
2023-12-24 23:29:33 -0500

    Update execution environment

--- TaskRipper.Core/IExecutionEnvironment.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TaskRipper.Core
-{
-    public interface IExecutionEnvironment
-    {
-        int ThreadCount { get; }
-    }
-}
---
+++ TaskRipper.Core/LocalExecutionEnvironment.cs
@@ -0,0 +1,76 @@
+﻿namespace TaskRipper.Core
+{
+    public interface IExecutionEnvironment
+    {
+        string MachineName { get; }
+        int ThreadCount { get; }
+    }
+    public sealed class LocalExecutionEnvironment : IExecutionEnvironment, IEquatable<LocalExecutionEnvironment>
+    {
+        private LocalExecutionEnvironment()
+            : this(Environment.MachineName, Environment.ProcessorCount)
+        { }
+
+        private LocalExecutionEnvironment(string machineName, int threadCount)
+        {
+            MachineName = Guard.Against.Null(machineName);
+            ThreadCount = threadCount;
+        }
+
+        public int ThreadCount { get; }
+        public string MachineName { get; }
+
+        /// <summary>
+        /// Returns an instance using the default constructor.
+        /// </summary>
+        public static IExecutionEnvironment Default { get; } = new LocalExecutionEnvironment();
+
+        public static IExecutionEnvironment Create(int threadCount, string machineName)
+        {
+            return new LocalExecutionEnvironment(machineName, threadCount);
+        }
+
+        public static IExecutionEnvironment FromThreadPool()
+        {
+            long threadPoolCount = GetThreadPoolCount();
+
+            // This cast will never fail since the min case will at least be int.MaxValue
+            int maxPossibleThreadPoolValue = (int)Math.Min(threadPoolCount, int.MaxValue);
+            return new LocalExecutionEnvironment(Environment.MachineName, maxPossibleThreadPoolValue);
+        }
+
+        private static long GetThreadPoolCount()
+        {
+            ThreadPool.GetAvailableThreads(out int availableWorkerThreads, out int completionPortThreads);
+
+            return Math.Max(availableWorkerThreads, 0);
+        }
+
+        public bool Equals(LocalExecutionEnvironment? other)
+        {
+            if (other is null)
+                return false;
+
+            return ThreadCount == other.ThreadCount;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+
+            if (obj is LocalExecutionEnvironment other)
+                return Equals(other);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine
+            (
+                ThreadCount
+            );
+        }
+    }
+}
