806ef525d75cddb8af8d66a387a80ff2e8948684
Anthony J <reapsprgm@gmail.com>
2024-02-04 20:42:46 -0500

    Update IWorkResult and concrete to have more props and inspect the nested results object

--- TaskRipper.Core/IWorkResult.cs
+++ TaskRipper.Core/IWorkResult.cs
@@ -1,62 +1,87 @@
-﻿//namespace TaskRipper.Core
-//{
-//    public interface IWorkResult<TResult> : IWorkResult
-//    {
-//        /// <summary>
-//        /// A collection of key/value pairs that contain the result of every iteration
-//        /// by every thread for a particular work.
-//        /// </summary>
-//        IEnumerable<IEnumerable<IterationResult<TResult>>> Results { get; }
-//    }
+﻿namespace TaskRipper.Core
+{
 
-//    public interface IWorkResult
-//    {
-//        /// <summary>
-//        /// The original contract used to generate this result.
-//        /// </summary>
-//        IWorkContract WorkContract { get; }
+    public interface IWorkResult<TResult>
+    {
+        IEnumerable<IEnumerable<TResult>> ResultsMatrix { get; }
+        /// <summary>
+        /// The original contract used to generate this result.
+        /// </summary>
+        IWorkContract OriginalContract { get; }
 
-//        /// <summary>
-//        /// The duration for this execution.
-//        /// </summary>
-//        TimeSpan Duration { get; }
+        bool HasCompleted { get; }
 
-//        /// <summary>
-//        /// The number of threads used for this specific task.
-//        /// </summary>
-//        int ThreadsUsed { get; }
-//    }
+        bool ContractHonored { get; }
 
-//    public class WorkResult : IWorkResult
-//    {
-//        public WorkResult(IWorkContract workContract, int threadsUsed)
-//        {
-//            WorkContract = workContract;
-//            ThreadsUsed = threadsUsed;
-//        }
-//        /// <inheritdoc/>
-//        public IWorkContract WorkContract { get; }
+        /// <summary>
+        /// The duration for this execution.
+        /// </summary>
+        TimeSpan Duration { get; }
 
-//        /// <inheritdoc/>
-//        public TimeSpan Duration { get; }
+        /// <summary>
+        /// The number of threads used for this specific task.
+        /// </summary>
+        int ThreadsUsed { get; }
+    }
 
-//        /// <inheritdoc/>
-//        public int ThreadsUsed { get; }
+    public sealed class WorkResult<TResult> : IWorkResult<TResult>
+    {
+        public WorkResult(IWorkContract workContract, int threadsUsed)
+        {
+            OriginalContract = workContract;
+            ThreadsUsed = threadsUsed;
+        }
+        /// <inheritdoc/>
+        public IWorkContract OriginalContract { get; init; }
 
-//        /// <summary>
-//        /// Inspect this task to see information on the overall task
-//        /// </summary>
-//        public Task ExecuterTask { get; }
-//    }
+        /// <inheritdoc/>
+        public TimeSpan Duration { get; init; }
 
-//    public class WorkResult<TResult> : WorkResult, IWorkResult<TResult>
-//    {
-//        public WorkResult(IWorkContract workContract, int threadsUsed, IEnumerable<IEnumerable<IterationResult<TResult>>> results) 
-//            : base(workContract, threadsUsed)
-//        {
-//            Results = results;
-//        }
-//        /// <inheritdoc/>
-//        public IEnumerable<IEnumerable<IterationResult<TResult>>> Results { get; }
-//    }
-//}
+        /// <inheritdoc/>
+        public int ThreadsUsed { get; init; }
+
+        /// <summary>
+        /// Inspect this task to see information on the overall task
+        /// </summary>
+        protected Task ExecuterTask { get; init; }
+
+        public IEnumerable<IEnumerable<TResult>> ResultsMatrix { get; init; }
+        public int PartitionCount => ResultsMatrix.Count();
+        public int TotalCount => ResultsMatrix.Sum(p => p.Count());
+
+        public bool HasCompleted => ExecuterTask.IsCompleted;
+
+        public bool ContractHonored => OriginalContract.IterationsRequested == TotalCount && HasCompleted;
+
+        public IEnumerable<TResult> FromPartition(int partitionIndex)
+        {
+            var range = new Range(0, PartitionCount);
+
+            if (!range.IsInRange(partitionIndex))
+            {
+                return Enumerable.Empty<TResult>();
+            }
+
+            return ResultsMatrix.ElementAt(partitionIndex);
+        }
+        public TResult? FromIndex(int partitionIndex, int index)
+        {
+            var partition = FromPartition(partitionIndex);
+            
+            if (partition == Enumerable.Empty<TResult>())
+            {
+                return default;
+            }
+
+            var count = partition.Count();
+            var range = new Range(0, count);
+
+            if (!range.IsInRange(index))
+            {
+                return default;
+            }
+
+            return partition.ElementAt(index);
+        }
+    }
+}
